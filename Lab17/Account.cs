980d14865f48fa3bf619dd784c115ffb5f02c822
Zakhar Galanin <zakhargalanin@gmail.com>
2022-07-23 15:00:22 -0700

    Add project files.

---
+++ Lab17/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    public class Account<T>
+    {
+        private T Id {get;set;}
+        private int Balance { get;set;}
+        private string Name {get;set;}
+
+        public string GetInfo()
+        {
+            return $"{Id} {Balance} {Name}";
+        }
+
+        public void SetInfo()
+        {
+            Console.WriteLine("Введите номер счета");
+            Id = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите баланс");
+            Balance = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            Name = Console.ReadLine();
+        }
+
+    }
+}
---
+++ Lab17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account<int> account1 = new Account<int>();
+            account1.SetInfo();
+            Console.WriteLine(account1.GetInfo());
+
+            Account<string> account2 = new Account<string>();
+            account2.SetInfo();
+            Console.WriteLine(account2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
